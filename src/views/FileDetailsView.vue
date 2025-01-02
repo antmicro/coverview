83d1d443de8ef61eb046e01ef60febc272e42ba2
Michael Gielda <mgielda@antmicro.com>
2025-01-02 06:43:06 +0100

    Add details view

--- src/views/FileDetailsView.vue
+++ src/views/FileDetailsView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useRoute } from "vue-router";
 import { store, countCoverageForLine } from '../store.js';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const params = useRoute().params;
 const file = computed(() => store.modules[params.moduleName].files[params.fileName]);
@@ -13,7 +13,6 @@ if (source) {
 } else {
   lineCount = Math.max(...(Object.values(file.value.coverage).map(x => Object.keys(x.lines || []).toSorted().at(-1) || 0)));
 }
-console.log(lineCount)
 
 function getColor(coverageData) {
   if (Object.keys(coverageData).length === 0) return "";
@@ -33,11 +32,16 @@ let lines = computed(() => Array.from(
           if (line) coverageData[type] = countCoverageForLine(line);
         }
       }
-      const lineData = { n: i+1, coverageData, color: getColor(coverageData) };
+      const lineData = { n: i+1, coverageData, color: getColor(coverageData), showDetails: ref('') };
       if (source) lineData.source = source[i];
       return lineData;
  })
 ));
+
+const toggleDetails = (line, type) => {
+  line.showDetails.value = (line.showDetails.value === type) ? '' : type;
+}
+
 </script>
 
 <template>
@@ -46,12 +50,14 @@ let lines = computed(() => Array.from(
     <table v-if="lines.length != 0">
       <thead><tr><th></th><th v-for="name in Object.keys(store.types)">{{ name }} data</th><th></th><th>Source code</th></tr></thead>
       <tbody>
-        <tr v-for="line in lines" :key="line.n">
+        <template v-for="line in lines" :key="line.n">
+        <tr>
           <td>{{ line.n }}</td>
-          <td v-for="type in Object.keys(store.types)" :class="line.color"><span v-if="line.coverageData[type] && store.types[type].visibility">{{ line.coverageData[type].hits }}/{{ line.coverageData[type].total }}</span></td>
+          <td v-for="type in Object.keys(store.types)" :class="line.color"><span v-if="line.coverageData[type] && store.types[type].visibility">{{ line.coverageData[type].hits }}/{{ line.coverageData[type].total }}</span><span style="padding-left: 5px; cursor: pointer" @click="toggleDetails(line, type)" v-if="line.coverageData[type] && store.types[type].visibility && file.coverage[type]?.lines[line.n].groups">{{ line.showDetails.value === type ? '[-]' : '[+]' }}</span></td>
           <td :class="line.color" style="color: #52525B;">:</td>
-          <td class="break" :class="line.color">{{ source ? line.source : 'NO LINE SOURCE AVAILABLE' }}</td>
+          <td class="break" :class="line.color">{{ source ? line.source : 'NO LINE SOURCE AVAILABLE' }}<div v-if="line.showDetails.value !== ''"><div v-for="g in file.coverage[line.showDetails.value]?.lines[line.n].groups"><div v-for="datapoint in g">{{ `${datapoint.info}: ${datapoint.value}` }}</div></div></div></td>
         </tr>
+      </template>
       </tbody>
     </table>
   </main>
